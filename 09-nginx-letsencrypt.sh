8618f6eb7b4158d293e55d45de82f992071abaf6
Jens <jens@hartlep.eu>
2016-02-22 23:08:42 +0100

    Update 09-nginx-letsencrypt.sh

--- 09-nginx-letsencrypt.sh
+++ 09-nginx-letsencrypt.sh
@@ -7,23 +7,19 @@ echo '|   |  \/ /_/  >  |   |  \>    <  |  |_\  ___/|  |  \___ \ |        \   | 
 echo '|___|  /\___  /|__|___|  /__/\_ \ |____/\___  >__| /____  >_______  /___|  /\___  >__|   / ____||   __/|__|  '
 echo '     \//_____/         \/      \/           \/          \/        \/     \/     \/       \/     |__|         '
 
-echo "
-location /.well-known/acme-challenge {
+echo "location /.well-known/acme-challenge {
 	location ~ /.well-known/acme-challenge/(.*) {
 		return	301	http://letsencrypt-auth.hartlep.email\$request_uri;
 	}
-}
-" | sudo tee /etc/nginx/conf.d/letsencrypt-auth.conf > /dev/null
+}" | sudo tee /etc/nginx/conf.d/letsencrypt-auth.conf > /dev/null
 
-echo "
-server {
+echo "server {
 	listen			80;
 	listen			[::]:80;
 	server_name		letsencrypt-auth.hartlep.email;
 	root			/var/www/letsencrypt/letsencrypt-auth;
 	default_type		text/plain;
-}
-" | sudo tee /etc/nginx/sites-available/letsencrypt > /dev/null
+}" | sudo tee /etc/nginx/sites-available/letsencrypt > /dev/null
 
 sudo mkdir -p /var/www/letsencrypt/letsencrypt-auth/.well-known/acme-challenge
 sudo ln -s /etc/nginx/sites-available/letsencrypt /etc/nginx/sites-enabled/001-letsencrypt
