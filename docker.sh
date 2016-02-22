9b678529dbc09b39a29903cb3f96e12fdbbf671e
Jens <jens@hartlep.eu>
2016-02-22 09:43:24 +0100

    Update docker.sh

--- docker.sh
+++ docker.sh
@@ -12,3 +12,5 @@ echo '        \/            \/     \/    \/     \/            '
 curl -fsSL https://get.docker.com/gpg | sudo apt-key add -
 curl -fsSL https://get.docker.com/ | sh
 sudo usermod -aG docker $USER
+
+mkdir ~/docker
