e78ee92c40bb87cd6ff580c9555836eadd2591c5
Jens <jens@hartlep.eu>
2016-02-22 22:52:48 +0100

    Update 05-openvpn.sh

--- 05-openvpn.sh
+++ 05-openvpn.sh
@@ -17,3 +17,12 @@ docker run --name $OVPN_DATA -v /home/$USER/docker/openvpn:/etc/openvpn busybox
 docker run --volumes-from $OVPN_DATA --rm kylemanna/openvpn ovpn_genconfig -u udp://vpn.quarkdose.de:1194
 docker run --volumes-from $OVPN_DATA --rm -it kylemanna/openvpn ovpn_initpki
 docker run --name $OVPN_SERVER --volumes-from $OVPN_DATA -d -p 1194:1194/udp --cap-add=NET_ADMIN kylemanna/openvpn
+
+echo "description \"OpenVPN Server container\"
+author \"Jens Hartlep\"
+start on filesystem and started docker
+stop on runlevel [!2345]
+respawn
+script
+  /usr/bin/docker start -a $OVPN_SERVER
+end script" | sudo tee /etc/init/docker-openvpn.conf > /dev/null
