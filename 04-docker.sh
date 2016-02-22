7c79b7b65140914680b0379bedd1c12c758125bb
Jens <jens@hartlep.eu>
2016-02-22 22:34:53 +0100

    Update 04-docker.sh

--- 04-docker.sh
+++ 04-docker.sh
@@ -9,6 +9,8 @@ echo ' |    `   (  <_> )  \___|    <\  ___/|  | \/ |  (  <_> )'
 echo '/_______  /\____/ \___  >__|_ \\___  >__| /\ |__|\____/ '
 echo '        \/            \/     \/    \/     \/            '
 
+sudo apt-get install aufs-tools linux-image-virtual linux-image-extra-virtual -y
+
 curl -fsSL https://get.docker.com/gpg | sudo apt-key add -
 curl -fsSL https://get.docker.com/ | sh
 sudo usermod -aG docker $USER
