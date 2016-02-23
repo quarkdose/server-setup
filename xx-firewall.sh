9b4adc09569a1a14afe4bcb5b492da03374365c8
Jens <jens@hartlep.eu>
2016-02-23 09:21:39 +0100

    Update xx-firewall.sh

--- xx-firewall.sh
+++ xx-firewall.sh
@@ -8,9 +8,18 @@ echo ' \___  /   |__||__|    \___  >\/\_/  (____  /____/____/'
 echo '     \/                    \/             \/           '
 
 sudo ufw allow ssh
+
 sudo ufw allow 1194/udp
+
 sudo ufw allow 80/tcp
 sudo ufw allow 443/tcp
+
+sudo ufw allow 25/tcp
+sudo ufw allow 110/tcp
+sudo ufw allow 143/tcp
+sudo ufw allow 587/tcp
+sudo ufw allow 993/tcp
+sudo ufw allow 995/tcp
 
 sudo ufw show added
 
