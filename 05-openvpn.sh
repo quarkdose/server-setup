851c64bafa6ea766710c370c8a35c0bddfd3a483
Jens <jens@hartlep.eu>
2016-02-23 11:08:06 +0100

    Update 05-openvpn.sh

--- 05-openvpn.sh
+++ 05-openvpn.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+if [[ -z "$1" ]]
+	then echo "You must provide a FQDN (e.g. vpn.example.com)"
+	exit
+fi
+
 echo '________                     ____   _____________________   '
 echo '\_____  \ ______   ____   ___\   \ /   /\______   \      \  '
 echo ' /   |   \\____ \_/ __ \ /    \   Y   /  |     ___/   |   \ '
@@ -14,7 +19,7 @@ OVPN_SERVER="OpenVPN-Server"
 USER=$(whoami)
 
 docker run --name $OVPN_DATA -v /home/$USER/docker/openvpn:/etc/openvpn busybox
-docker run --volumes-from $OVPN_DATA --rm kylemanna/openvpn ovpn_genconfig -u udp://vpn.quarkdose.de:1194
+docker run --volumes-from $OVPN_DATA --rm kylemanna/openvpn ovpn_genconfig -u udp://$1:1194
 docker run --volumes-from $OVPN_DATA --rm -it kylemanna/openvpn ovpn_initpki
 docker run --name $OVPN_SERVER --volumes-from $OVPN_DATA -d -p 1194:1194/udp --cap-add=NET_ADMIN kylemanna/openvpn
 
