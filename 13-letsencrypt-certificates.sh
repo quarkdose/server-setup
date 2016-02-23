b0ae67615ff18c6461d89b5613a75a4a24072218
Jens <jens@hartlep.eu>
2016-02-23 19:25:45 +0100

    Update 13-letsencrypt-certificates.sh

--- 13-letsencrypt-certificates.sh
+++ 13-letsencrypt-certificates.sh
@@ -1,20 +1,90 @@
 #!/bin/bash
 
 if [[ -z "$1" ]]
-	then echo "You must provide a alt least one domain (e.g. example.com)"
-	exit
+        then echo "You must provide a countryName"
+        exit
 fi
 
-echo '.____           __ /\        ___________                                   __    _________                __  .__  _____.__               __                 '
-echo '|    |    _____/  |)/ ______ \_   _____/ ____   ___________ ___.__._______/  |_  \_   ___ \  ____________/  |_|__|/ ____\__| ____ _____ _/  |_  ____   ______'
-echo '|    |  _/ __ \   __\/  ___/  |    __)_ /    \_/ ___\_  __ <   |  |\____ \   __\ /    \  \/_/ __ \_  __ \   __\  \   __\|  |/ ___\\__  \\   __\/ __ \ /  ___/'
-echo '|    |__\  ___/|  |  \___ \   |        \   |  \  \___|  | \/\___  ||  |_> >  |   \     \___\  ___/|  | \/|  | |  ||  |  |  \  \___ / __ \|  | \  ___/ \___ \ '
-echo '|_______ \___  >__| /____  > /_______  /___|  /\___  >__|   / ____||   __/|__|    \______  /\___  >__|   |__| |__||__|  |__|\___  >____  /__|  \___  >____  >'
-echo '        \/   \/          \/          \/     \/     \/       \/     |__|                  \/     \/                              \/     \/          \/     \/ '
+if [[ -z "$2" ]]
+        then echo "You must provide a stateOrProvinceName"
+        exit
+fi
 
-for domain in "$@"
-do
-	domains+="-d $domain "
-done
+if [[ -z "$3" ]]
+        then echo "You must provide a localityName"
+        exit
+fi
 
-letsencrypt certonly --webroot -w /var/www/letsencrypt/letsencrypt-auth $domains
+if [[ -z "$4" ]]
+        then echo "You must provide a postalCode"
+        exit
+fi
+
+if [[ -z "$5" ]]
+        then echo "You must provide a streetAddress"
+        exit
+fi
+
+if [[ -z "$6" ]]
+        then echo "You must provide a organizationName"
+        exit
+fi
+
+if [[ -z "$7" ]]
+        then echo "You must provide a organizationalUnitName"
+        exit
+fi
+
+if [[ -z "$8" ]]
+        then echo "You must provide a commonName"
+        exit
+fi
+
+if [[ -z "$9" ]]
+        then echo "You must provide a emailAddress"
+        exit
+fi
+
+if [[ -z "$10" ]]
+        then echo "You must provide a subjectAltName (e.g. DNS:example.com,DNS:www.example.com)"
+        exit
+fi
+
+echo '.____           __ /\        ___________                                   __                 ________      _____    _______  ___________'
+echo '|    |    _____/  |)/ ______ \_   _____/ ____   ___________ ___.__._______/  |_      .__      \______ \    /  _  \   \      \ \_   _____/'
+echo '|    |  _/ __ \   __\/  ___/  |    __)_ /    \_/ ___\_  __ <   |  |\____ \   __\   __|  |___   |    |  \  /  /_\  \  /   |   \ |    __)_ '
+echo '|    |__\  ___/|  |  \___ \   |        \   |  \  \___|  | \/\___  ||  |_> >  |    /__    __/   |    `   \/    |    \/    |    \|        \'
+echo '|_______ \___  >__| /____  > /_______  /___|  /\___  >__|   / ____||   __/|__|       |__|     /_______  /\____|__  /\____|__  /_______  /'
+echo '        \/   \/          \/          \/     \/     \/       \/     |__|                               \/         \/         \/        \/ '
+
+mkdir -p ~/build/dane
+cd ~/build/dane
+
+echo "[ req ]
+default_md = sha512
+prompt = no
+encrypt_key = no
+distinguished_name = req_distinguished_name
+req_extensions = v3_req
+
+[ req_distinguished_name ]
+countryName = \"$1\"
+stateOrProvinceName = \"$2\"
+localityName = \"$3\"
+postalCode = \"$4\"
+streetAddress = \"$5\"
+organizationName = \"$6\"
+organizationalUnitName = \"$7\"
+commonName = \"$8\"
+emailAddress = \"$9\"
+
+[ v3_req ]
+subjectAltName = ${10}" | tee ~/build/dane/request.cnf > /dev/null
+
+openssl genrsa -out privkey.pem 4096
+openssl req -config request.cnf -new -key privkey.pem -out request.csr -outform der
+
+letsencrypt certonly --webroot -w /var/www/letsencrypt/letsencrypt-auth --key-path privkey.pem --cert-path cert.pem --fullchain-path fullchain.pem --csr request.csr
+
+sudo ln -s ~/build/dane/0000_fullchain.pem /etc/ssl/mail/mail.crt
+sudo ln -s ~/build/dane/privkey.pem /etc/ssl/mail/mail.key
