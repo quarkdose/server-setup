bd6e94832095f422048e60f62359bd42b584485c
Jens <jens@hartlep.eu>
2016-02-23 21:28:30 +0100

    Update 10-nginx-mailcow.sh

--- 10-nginx-mailcow.sh
+++ 10-nginx-mailcow.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+if [[ -z "$1" ]]
+        then echo "You must provide a domain name (e.g. example.tld)"
+        exit
+fi
+
 echo '               .__                   _____         .__.__  _________                '
 echo '  ____    ____ |__| ____ ___  ___   /     \ _____  |__|  | \_   ___ \  ______  _  __'
 echo ' /    \  / ___\|  |/    \\  \/  /  /  \ /  \\__  \ |  |  | /    \  \/ /  _ \ \/ \/ /'
@@ -7,13 +12,13 @@ echo '|   |  \/ /_/  >  |   |  \>    <  /    Y    \/ __ \|  |  |_\     \___(  <_
 echo '|___|  /\___  /|__|___|  /__/\_ \ \____|__  (____  /__|____/\______  /\____/ \/\_/  '
 echo '     \//_____/         \/      \/         \/     \/                \/               '
 
-sudo cp ~/backup/nginx/sites-available/mailcow /etc/nginx/sites-available
+sudo cp ~/backup/nginx/sites-available/mailcow /etc/nginx/sites-available/mailcow.$1
 sudo sed -i 's/listen 443;/listen 443 ssl http2;/g' /etc/nginx/sites-available/mailcow
 sudo sed -i 's/listen \[::\]:443;/listen \[::\]:443 ssl http2;/g' /etc/nginx/sites-available/mailcow
 sudo sed -i '/ssl on;/i \\tinclude conf.d/letsencrypt-auth.conf;' /etc/nginx/sites-available/mailcow
 sudo sed -i '/server {/,/}/ { // { x; s/$/./; x; }; x; /.../! { x; d; }; x; }' /etc/nginx/sites-available/mailcow
 
-sudo ln -s /etc/nginx/sites-available/mailcow /etc/nginx/sites-enabled/003-mailcow
+sudo ln -s /etc/nginx/sites-available/mailcow.$1 /etc/nginx/sites-enabled/003-mailcow.$1
 
 sudo nginx -t
 
